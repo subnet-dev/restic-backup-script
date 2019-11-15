6de8eddb4b1768807aea111c8938183310c20929
subnet.dev <50828879+subnet-dev@users.noreply.github.com>
2019-11-15 15:37:29 +0100

    Create the check_connection function

--- restic-backup-script.sh
+++ restic-backup-script.sh
@@ -3,3 +3,20 @@
 #### Script Constant ####
 SCRIPT_FILE_PATH=$0
 SCRIPT_DIR_PATH=$(dirname $SCRIPT_FILE_PATH)
+
+#### Functions ####
+# Function to check if some ip is accessible
+function check_connection() {
+  if [[ ! -z $1 ]]; then
+    ping -t 2 -c 1 $1 &> /dev/null
+    ping_result=$?
+    if [[ $ping_result == "0" ]]; then
+      echo 1
+    else
+      echo 0
+    fi
+  else
+    echo 0
+  fi
+}
+
